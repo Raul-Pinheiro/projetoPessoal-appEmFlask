7e6100ebfbf9286b77afc25c7a42916c64b1907e
Raul-Pinheiro <raulmatheus.rm@gmail.com>
2021-02-16 14:48:26 -0300

    Projeto em Flask - inicio

---
+++ manage.py
@@ -0,0 +1,9 @@
+from rotas.index import *
+from static import *
+
+
+if __name__ == '__main__':    
+    app.run(debug=True)
+
+
+
---
+++ rotas/index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,template_folder='../templates')
+
+@app.route("/")
+def home():
+    title='Home' 
+    return render_template('home.html', title=title)
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    title='Dashboard' 
+
+    return render_template('dashboard.html', title=title)
